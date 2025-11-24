69a4df3891843647a2e5fe194924ae100c41cd1d
Philamand <contact@autobacklog.com>
2025-11-24 13:07:02 +0000

    Basics Book and Page models

--- book/models.py
+++ book/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    cover = models.ImageField(upload_to="books/covers/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Page(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    text = models.TextField()
+    image = models.ImageField(upload_to="books/images/")
+    audio = models.FileField(upload_to="books/audio/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

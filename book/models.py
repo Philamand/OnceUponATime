b36209d53e658fb7ada5c93e54bb4643f9632edc
Philamand <contact@autobacklog.com>
2025-12-01 09:39:06 +0000

    Added BaseModel

--- book/models.py
+++ book/models.py
@@ -2,25 +2,29 @@ from django.db import models
 from django.urls import reverse
 
 
-class Book(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Book(BaseModel):
     title = models.CharField(max_length=200)
     description = models.TextField()
     cover = models.ImageField(upload_to="books/covers/")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     published = models.BooleanField(default=False)
-
-    def get_absolute_url(self) -> str:
-        return reverse("book_detail", kwargs={"pk": self.pk})
 
     def __str__(self) -> str:
         return self.title
 
+    def get_absolute_url(self) -> str:
+        return reverse("book_detail", kwargs={"pk": self.pk})
 
-class Page(models.Model):
+
+class Page(BaseModel):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     text = models.TextField()
     image = models.ImageField(upload_to="books/images/")
     audio = models.FileField(upload_to="books/audio/")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)

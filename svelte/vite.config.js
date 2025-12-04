31b193fa9474cea6dae29ebff7ac885eb2c04e59
Philamand <contact@autobacklog.com>
2025-12-04 13:53:32 +0000

    Cleaned Svelte project

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -4,4 +4,20 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        'book_detail': './src/book_detail.js',
+      },
+      output: {
+        entryFileNames: '../static/[name]-[hash].js',
+        chunkFileNames: '../static/[name]-[hash].js',
+        assetFileNames: '../static/[name].[ext]'
+      }
+    }
+  },
+  server: {
+    host: true,
+    port: 5173
+  }
 })

3cd1df0b07b35cd0d352d4da85956b1d813373d5
Philamand <contact@autobacklog.com>
2025-12-10 13:44:33 +0000

    Created Documentary model

--- documentary/models.py
+++ documentary/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Documentary(BaseModel):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="documentaries/")
+    text = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
